1832f32d40c76825e8b9c667fb805a636eea1e3e
itscaiosantos <83199605+itscaiosantos@users.noreply.github.com>
2022-09-13 11:14:21 -0500

    Create portal-professor.php

---
+++ portal-professor.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/portal-aluno.css">
+    <title>SICOHV</title>
+</head>
+<body>
+    <div class="cabecalho">
+        <div id="logo">
+            <img src="img/if.jpeg">
+        </div>
+        <div id="portal">
+            <h1>Portal do Professor</h1>
+        </div>
+    </div>
+    <div>
+        <?php
+            
+        ?>
+    </div>
+</body>
+</html>
