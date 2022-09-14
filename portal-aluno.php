a24cbaa6f312d37adddae8204e1308cc671eacc2
Ezequiel0234 <106176911+Ezequiel0234@users.noreply.github.com>
2022-09-14 08:38:22 -0500

    atualização do portal aluno

--- portal-aluno.php
+++ portal-aluno.php
@@ -4,22 +4,51 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/padrao.css">
     <link rel="stylesheet" href="css/portal-aluno.css">
     <title>SICOHV</title>
 </head>
 <body>
-    <div class="cabecalho">
-        <div id="logo">
-            <img src="img/if.jpeg">
+    <div class="row">
+        <div class="col-25" id="logo">
+            <img src="img/if.jpeg" width="90px">
         </div>
-        <div id="portal">
-            <h1>Portal do aluno</h1>
+        <div class="col-75">
+            <h2 id="titulo">Portal do aluno</h2>
         </div>
     </div>
-    <div>
+    <div class="row" id="turma">
+        <h2>IPI - 3°</h2>
+    </div>
+    <div class="row">
         <?php
-            
+
+            // dados para conexão com banco
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $database = "turmas";
+
+            // criar conexão com o banco
+            $conn = new mysqli($servername, $username, $password);
+
+            // checar conexão
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
         ?>
+        <table>
+            <tr>
+                <th>hora</th>
+                <th>seg</th>
+                <th>ter</th>
+                <th>qua</th>
+                <th>qui</th>
+                <th>sex</th>
+                <th>sab</th>
+            </tr>
+        </table>
     </div>
 </body>
 </html>
