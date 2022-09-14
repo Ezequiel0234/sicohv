2081712b584e225c51d14af8550342743665d960
Ezequiel0234 <106176911+Ezequiel0234@users.noreply.github.com>
2022-09-14 07:48:09 -0500

    base de dados a ser usada pelo sistema

--- turmas.sql
+++ turmas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 08-Set-2022 às 17:34
+-- Tempo de geração: 13-Set-2022 às 19:15
 -- Versão do servidor: 10.4.11-MariaDB
--- versão do PHP: 7.4.1
+-- versão do PHP: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -38,6 +38,22 @@ CREATE TABLE `ipi3` (
   `sab` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `ipi3`
+--
+
+INSERT INTO `ipi3` (`hora`, `seg`, `ter`, `qua`, `qui`, `sex`, `sab`) VALUES
+('07:00', 1, 2, 12, 3, 5, 0),
+('07:50', 1, 2, 12, 3, 5, 0),
+('08:40', 3, 13, 6, 7, 9, 0),
+('09:50', 3, 13, 6, 7, 10, 0),
+('10:40', 4, 8, 11, 4, 10, 0),
+('11:30', 4, 8, 11, 4, 2, 0),
+('14:20', 5, 0, 3, 0, 0, 0),
+('15:10', 5, 0, 3, 0, 0, 0),
+('16:20', 0, 0, 4, 0, 0, 0),
+('17:10', 0, 0, 4, 0, 0, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +64,26 @@ CREATE TABLE `materias` (
   `id` int(11) NOT NULL,
   `materia` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `materias`
+--
+
+INSERT INTO `materias` (`id`, `materia`) VALUES
+(0, 'Livre'),
+(1, 'Empreendedorismo'),
+(2, 'Língua Portuguesa'),
+(3, 'Matemática'),
+(4, 'Física'),
+(5, 'Química'),
+(6, 'Biologia'),
+(7, 'História1'),
+(8, 'Geografia'),
+(9, 'Filosofia'),
+(10, 'Sociologia'),
+(11, 'Facultativa (Espanhol/Francês)'),
+(12, 'Prática em Programação WEB'),
+(13, 'Segurança de Aplicação WEB');
 
 --
 -- Índices para tabelas despejadas
