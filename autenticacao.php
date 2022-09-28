4e0ea4d7841b47f6b3e4ce6af1090ab1b127f8ae
fabisussuarana <106176566+fabisussuarana@users.noreply.github.com>
2022-09-28 07:59:08 -0500

    Feita a validação dos professores

--- autenticacao.php
+++ autenticacao.php
@@ -16,6 +16,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// sql pra selecionar o usuario e senha
+$sql = "SELECT * FROM profs WHERE usuario = '$usuario' AND senha = '$senha';";
 
+$result = $conn->query($sql);
+
+// Verifica se tem uma ou mais linhas na matriz
+if (mysqli_num_rows($result) > 0){
+    include 'portal-professor.php';
+}
+else{
+    die("Erro, seu usuário ou senha está incorreto!");
+}
 
 ?>
