a59ebce09ad4c2ef853521f589931d92d7404644
fabisussuarana <106176566+fabisussuarana@users.noreply.github.com>
2022-09-28 07:35:57 -0500

    Adicionei a senha root para que a conexão funcionasse

--- php/conexao.php
+++ php/conexao.php
@@ -3,7 +3,7 @@
     // Dados para conexão com banco
     $servername = "localhost";
     $username = "root";
-    $password = "";
+    $password = "root";
     $database = "turmas";
 
     // Criar conexão com o banco
